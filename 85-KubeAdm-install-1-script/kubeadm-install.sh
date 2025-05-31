f65ba5784b0df6f97bfc1571780508a9fd7b4a43
ticveintitres <admin@ticveintitres.local>
2025-05-31 21:45:14 +0200

    definitivo

--- 85-KubeAdm-install-1-script/kubeadm-install.sh
+++ 85-KubeAdm-install-1-script/kubeadm-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-## Script hecho con amor por Alejandro Martínez Capilla (@ticveintitres)
+## Script hecho con amor por Alejandro Martinez Capilla (@ticveintitres)
 
-## Instalación automatizada de Kubernetes + CRI-O + Calico
+## Instalacion automatizada de Kubernetes + CRI-O + Calico
 
-##Declaramos las varibles para que sea más fácil de instalar.
+##Declaramos las varibles para que sea mas facil de instalar.
 ##-----------------------------------------------------------------------------------
 
 ##Declaramos las variables
@@ -22,7 +22,7 @@ CALICO_VERSION=v3.30.0
 
 ##### INSTALANDO CONTROLPLANE
 
-##Actualizamos y instalamos paquetería.
+##Actualizamos y instalamos paqueteria.
 ##-------------------------------------------------------
 apt update || { echo "Error actualizando paquetes"; exit 1; }
 apt install -y software-properties-common curl
@@ -35,7 +35,7 @@ echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.o
 apt update
 apt install -y cri-o kubelet kubeadm kubectl
 
-##Arrancamos los servicios, quitamos el SWAP y añadimos reglas.
+##Arrancamos los servicios, quitamos el SWAP ..etc
 ##------------------------------------------------------------------------------------------
 systemctl start crio.service
 systemctl enable crio.service
@@ -47,6 +47,10 @@ sysctl -w net.ipv4.ip_forward=1
 ##Reiniciar CRIO.
 ##---------------------
 systemctl restart crio.service
+
+# Bashcompletion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'source <(kubeadm completion bash)' >>~/.bashrc
 
 ##Instalamos el cluster de KUBEADM.
 ##--------------------------------------------------
@@ -77,20 +81,30 @@ JOIN_CMD=$(kubeadm token create --print-join-command)
 
 ## Configurar igual que en el Controlplane
 
-ssh "$NODE_USER@$NODE_IP" "apt update || { echo "Error actualizando paquetes"; exit 1; } && \
-apt install -y software-properties-common curl && \
-curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list && \
-curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg && \
-echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" | tee /etc/apt/sources.list.d/cri-o.list && \
-apt update && \
-apt install -y cri-o kubelet kubeadm kubectl && \
-systemctl start crio.service && \
-systemctl enable crio.service && \
-swapoff -a && \
-sed -i.bak '/ swap / s/^/#/' /etc/fstab && \
-modprobe br_netfilter && \
-sysctl -w net.ipv4.ip_forward=1"
+ssh "$NODE_USER@$NODE_IP" bash -s <<EOF
+KUBERNETES_VERSION=v1.33
+CRIO_VERSION=v1.33
+apt update || { echo "Error actualizando paquetes"; exit 1; }
+apt install -y software-properties-common curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$CRIO_VERSION/deb/ /" | tee /etc/apt/sources.list.d/cri-o.list
+apt update
+apt install -y cri-o kubelet kubeadm kubectl
+systemctl start crio.service
+systemctl enable crio.service
+swapoff -a
+sed -i.bak '/ swap / s/^/#/' /etc/fstab
+modprobe br_netfilter
+sysctl -w net.ipv4.ip_forward=1
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'source <(kubeadm completion bash)' >>~/.bashrc
+EOF
 
-# Añadimos el nodo al cluster
-ssh "$NODE_USER@$NODE_IP" "$JOIN_CMD"
+# Join el nodo al cluster
+ssh "$NODE_USER@$NODE_IP" bash -s <<EOF
+$JOIN_CMD
+EOF
+
+bash
